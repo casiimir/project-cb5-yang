d69e00d72cae1c48c9f72bef2840e4a8c19d0974
flgisimone <florestagiuliosimone@gmail.com>
2023-02-15 16:36:05 +0100

    aggiunta link per tornare alla home

--- src/pages/top_artist.jsx
+++ src/pages/top_artist.jsx
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar"
 import Image from "next/image"
+import Link from "next/link";
+import { MdArrowBackIos } from 'react-icons/md';
 import styles from "./top_artist/styles.module.scss"
 
 const Top_Artist = ({data}) => {
   return (
     <>
     <div className={styles.TopArtist}>
+    <Link href={"/"}><MdArrowBackIos /> ALL ARTISTS</Link>
       {data?.data
       .map((artist) => (
         <div className={styles.container_Content}>
