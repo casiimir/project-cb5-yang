681954006bb63f87b9eba35d7cbddd233ea9b420
Dario Purpi <dariopurpi@gmail.com>
2023-02-16 16:18:53 +0100

    fix method to login

--- src/pages/single_album/[id].jsx
+++ src/pages/single_album/[id].jsx
@@ -1,52 +1,53 @@
 import Image from "next/image";
 import Link from "next/link";
-import { MdArrowBackIos } from 'react-icons/md';
+import { MdArrowBackIos } from "react-icons/md";
 
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
 export default function SigleAlbum({ albumData }) {
-  
-    return (
-      <div className={styles.Album}>
-        <Link className={styles.linkAlbum} href={"/top_album"}>
-          <MdArrowBackIos /> ALL ALBUMS
-        </Link>
-        <div className={styles.containerAlbum}>
-          <Image src={albumData.cover_medium} width={250} height={250} alt={albumData.title} />
-          <h1>{albumData.title}</h1>
-          <h5>{albumData.artist.name}</h5>
-          <div className={styles.Tracks}>
-            {
-                albumData?.tracks?.data?.map(track => 
-                <div className={styles.singleTrack}>
-                  <span>{track.title_short}</span>
-                  <audio controls id="my-audio">
-                    <source src={track.preview} type="audio/mp3" />
-                  </audio>
-                </div>
-                )
-            }
-          </div>
+  return (
+    <div className={styles.Album}>
+      <Link className={styles.linkAlbum} href={"/top_album"}>
+        <MdArrowBackIos /> ALL ALBUMS
+      </Link>
+      <div className={styles.containerAlbum}>
+        <Image
+          src={albumData.cover_medium}
+          width={250}
+          height={250}
+          alt={albumData.title}
+        />
+        <h1>{albumData.title}</h1>
+        <h5>{albumData.artist.name}</h5>
+        <div className={styles.Tracks}>
+          {albumData?.tracks?.data?.map((track) => (
+            <div className={styles.singleTrack}>
+              <span>{track.title_short}</span>
+              <audio controls id="my-audio">
+                <source src={track.preview} type="audio/mp3" />
+              </audio>
+            </div>
+          ))}
         </div>
       </div>
-    );
-  }
-  
-  export async function getStaticPaths() {
-    const res = await fetch("https://api.deezer.com/chart/0/albums");
-    const data = await res.json();
+    </div>
+  );
+}
 
-    const paths = data.data.map((album) => ({
-      params: { id: album.id.toString() },
-    }));
-  
-    return { paths, fallback: true };
-  }
-  
-  export async function getStaticProps({ params }) {
+export async function getStaticPaths() {
+  const res = await fetch("https://api.deezer.com/chart/0/albums");
+  const data = await res.json();
 
-    const res = await fetch(`https://api.deezer.com/album/${params.id}`);
-    const albumData = await res.json();
+  const paths = data.data.map((album) => ({
+    params: { id: album.id.toString() },
+  }));
 
-    return { props: { albumData } };
-  }
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`https://api.deezer.com/album/${params.id}`);
+  const albumData = await res.json();
+
+  return { props: { albumData } };
+}
