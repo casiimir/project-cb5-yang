1041389ff1baa000fbca9776f6bcf988eb9bd1c6
Dario Purpi <dariopurpi@gmail.com>
2023-02-18 10:24:41 +0100

    remove-header-navbar-in-login

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,15 +1,25 @@
 import Header from "@/components/Header";
+import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 
 const MainLayout = ({ children }) => {
-  return (
-    <>
-      <Header />
-      {children}
+  const [logged, setLogged] = useState("true");
+  useEffect(() => {
+    setLogged(localStorage.getItem("logged"));
+  }, []);
+  console.log(logged);
+  if (logged === "true") {
+    return (
+      <>
+        <Header />
+        {children}
 
-      <Navbar />
-    </>
-  );
+        <Navbar />
+      </>
+    );
+  } else {
+    return <> {children};</>;
+  }
 };
 
 export default MainLayout;
