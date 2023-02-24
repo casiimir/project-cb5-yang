3233c3a115f08174c3d5aa25de490ba4804ad87e
Dario Purpi <dariopurpi@gmail.com>
2023-02-24 22:53:00 +0100

    fix bug

--- src/pages/single_track/[id].jsx
+++ src/pages/single_track/[id].jsx
@@ -83,7 +83,7 @@ export default function SingleTrack({ trackData }) {
       }
     }
   };
-  // cerco l'artista corrente e il relativo valore di favoritee
+  // cerco l'artista corrente e il relativo valore di favorite
   const currentArtist = state?.favorite?.find(
     (item) => item.titleTrack === trackData.title
   );
